04016f8e34c70556176a4f4aa529efe7230753ad
Ankit Kumar Jha <140503877+ankit442660@users.noreply.github.com>
2024-09-17 18:41:58 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,7 @@ const contactRoute=require("./Router/contact-router");
 const serviceRoute=require("./Router/service-router");
 const adminRoute=require("./Router/admin-router")
 const corsOptions={
-    origin:"http://localhost:5173",
+    origin:"https://coding-hub-frontend.vercel.app",
     method:"GET, PUT, POST, DELETE, PATCH, HEAD",
     credentials:true,
 };
