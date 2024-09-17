a648f4e2660eeb0da535a8bbff6720f730b3211b
Ankit Kumar Jha <140503877+ankit442660@users.noreply.github.com>
2024-09-17 18:33:08 +0530

    Update AdminUsers.jsx

--- client/src/pages/AdminUsers.jsx
+++ client/src/pages/AdminUsers.jsx
@@ -10,7 +10,7 @@ export const AdminUsers = () => {
 
   const getAllUsersData = async () => {
     try {
-      const response = await fetch(`http://localhost:5000/api/admin/users`, {
+      const response = await fetch(`https://coding-hub-backend.vercel.app/api/admin/users`, {
         method: "GET",
         headers: {
           Authorization: authorization,
@@ -28,7 +28,7 @@ export const AdminUsers = () => {
   const deleteUser = async (id) => {
     try {
       const response = await fetch(
-        `http://localhost:5000/api/admin/users/delete/${id}`,
+        `https://coding-hub-backend.vercel.app/api/admin/users/delete/${id}`,
         {
           method: "DELETE",
           headers: {
